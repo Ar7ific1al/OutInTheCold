22e1af94a016c67bb45ca7914f5c2b2bad74ef0c
Ar7ific1al <ar7ific1al@gmail.com>
2014-01-28 04:02:11 -0600

    Added brief explanation to PlayerLoginListener

--- src/com/github/ar7ific1al/outinthecold/listeners/PlayerLoginListener.java
+++ src/com/github/ar7ific1al/outinthecold/listeners/PlayerLoginListener.java
@@ -20,7 +20,15 @@ public class PlayerLoginListener implements Listener{
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event){
-		long interval = 90;
+		/**
+		 * <Explanation>
+		 * When a player joins the server, an instance of PlayerPositionScheduler is created.
+		 * Passed into this object's constructor are parameters for the player who logged in, the plugin instance,
+		 * the start delay, and the interval for the repeating timer ("interval"). Every "interval" ticks (90 by default,
+		 * or 4.5 seconds), the server will fire off the code within run() in the PlayerPositionScheduler class.
+		 * </Explanation>
+		 */
+		long interval = plugin.Settings.getLong("Timer Settings.Player.Position");
 		PlayerPositionScheduler posTimer = new PlayerPositionScheduler(event.getPlayer(), plugin, 5, interval, interval);
 	}
 
